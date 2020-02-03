2aad8116b63d6f22b53fab5a702ec955ee748be2
iVan <i@i>
2020-02-03 14:27:07 +0200

    Added a basic, almost parent-like, configure method to the SecurityAdapter  - requires "ADMIN" role for /users/delete/**

--- src/main/java/com/denchevgod/security/config/WebSecurity.java
+++ src/main/java/com/denchevgod/security/config/WebSecurity.java
@@ -3,6 +3,7 @@ package com.denchevgod.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,23 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                     .withUser("user")
                     .password(passwordEncoder().encode("pass"))
                     .roles("USER");
+    } // @formatter:on
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception { // @formatter:off
+        http
+                .authorizeRequests()
+                    .antMatchers("/users/delete/**").hasAnyRole("ADMIN")
+                    .anyRequest()
+                        .authenticated()
+                    .and()
+
+                .formLogin()
+                    .and()
+
+                .httpBasic()
+
+        ;
     } // @formatter:on
 
     @Bean
