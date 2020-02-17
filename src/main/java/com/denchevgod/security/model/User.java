ee26cc448b310e5c18324b21703af72b99b1c0d7
iVan <i@i>
2020-02-17 12:28:58 +0200

    Added basic Hibernate annotations Changed persistence from InMemory to MySQL

--- src/main/java/com/denchevgod/security/model/User.java
+++ src/main/java/com/denchevgod/security/model/User.java
@@ -3,18 +3,21 @@ package com.denchevgod.security.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Entity
 @Getter
 @Setter
+@Entity
+@Table(name = "users")
 public class User {
 
     @Id
@@ -23,6 +26,7 @@ public class User {
 
     @NotNull(message = "Username Cannot be null")
     @Size(min = 1, message = "Username is required")
+    @Column(name = "username", unique = true)
     private String username;
 
     private String password;
@@ -30,6 +34,7 @@ public class User {
     @NotNull(message = "Age Cannot be null")
     @Min(value = 0, message = "Must be above 0")
     @Max(value = 100, message = "Must be below 100")
+    @Column(name = "age")
     private Integer age;
 
 }
